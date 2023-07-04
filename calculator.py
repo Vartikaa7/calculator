0b03aaa59acecb05baabdc2af5df1736280eb2a2
Vartikaa7 <138459290+Vartikaa7@users.noreply.github.com>
2023-07-04 10:51:58 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def calculate(num1,num2,operator):
+ if(operator =="+"):
+  return num1 + num2
+ if(operator =="-"):
+  return num1 - num2
+ if(operator =="*"):
+  return num1 * num2
+ if(operator =="/"):
+  return num1 / num2
+a=int(input("Input number1:"))
+b=int(input("Input number2:"))
+o=input("Input operator:")
+c=calculate(a,b,o)
+print(c)
